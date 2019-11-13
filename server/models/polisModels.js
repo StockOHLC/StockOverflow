eaff8138be86d1844da4797f166133a03ff08983
JoshuaManto <joshuamanto@yahoo.com>
2019-11-12 20:42:44 -0800

    Refactored date for buy schema

--- server/models/polisModels.js
+++ server/models/polisModels.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const MONGO_URI =
-  'mongodb+srv://sungdo1004:codesmith1004@cluster0-2sjwa.mongodb.net/test?retryWrites=true&w=majority';
+  "mongodb+srv://bradleyDB:kiroismypartner@cluster0-klwdv.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGO_URI, {
@@ -9,7 +9,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // sets the name of the DB that our collections are part of
-    dbName: 'stock'
+    dbName: "stock"
   })
   .then(() => console.log(`Connected to Sung's Mongo DB`))
   .catch(err => console.log(err));
@@ -24,27 +24,31 @@ const userSchema = new Schema({
   favorites: [String]
 });
 
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model("user", userSchema);
 
-const buySchema = new Schema({
-  email_address: String,
-  boughtStockId: String,
-  date: String,
-  purchasedPrice: Number,
-  numberOfShares: Number,
-  prediction: Number
-});
+const buySchema = new Schema(
+  {
+    email_address: String,
+    boughtStockId: String,
+    purchasedPrice: Number,
+    numberOfShares: Number,
+    prediction: Number
+  },
+  {
+    timestamps: true
+  }
+);
 
-const Buy = mongoose.model('buy', buySchema);
+const Buy = mongoose.model("buy", buySchema);
 
 const pastStockSchema = new Schema({
   stockSymbol: String,
   changes: [Object]
 });
-const PastStock = mongoose.model('pastStocks', pastStockSchema);
+const PastStock = mongoose.model("pastStocks", pastStockSchema);
 
 module.exports = {
-    User, 
-    Buy, 
-    PastStock
+  User,
+  Buy,
+  PastStock
 };
