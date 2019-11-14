c1df357f9a8eaf423dc400fe5e8c541f774cad17
evanking11 <evanking11@gmail.com>
2019-11-13 17:10:46 -0800

    fixed git issues

--- client/components/News.jsx
+++ client/components/News.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
-const News = props => {
-  return (
-    <div>
-      <div>This is where the news will be</div>
-    </div>
-  );
-};
+class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/news/topNews")
+      .then(result => result.json())
+      .then(result => {
+        this.setState({ news: result });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const newsArray = [];
+    for (let i = 0; i < this.state.news.length; i++) {
+      newsArray.push(
+        <div className="article">
+          <a href={this.state.news[i].url} key={"key " + i}>
+            {this.state.news[i].title}
+          </a>
+          <div>{this.state.news[i].content}</div>
+        </div>
+      );
+    }
+
+    return <div id="newsFeed">{newsArray}</div>;
+  }
+}
 
 export default News;
